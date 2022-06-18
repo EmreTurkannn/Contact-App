d9388254a872e16e0ad47c583f085478a3829366
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-19 00:50:01 +0300

    react-router  setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import react from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css';
+import About from './pages/About';
+import AddEdit from './pages/AddEdit';
+import Home from './pages/Home';
+import View from './pages/View';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={Home}></Route>
+        <Route path="/add" element={AddEdit}></Route>
+        <Route path="/uptade/id" element={AddEdit}></Route>
+        <Route path="/view/id" element={View}></Route>
+        <Route path="/about" element={About}></Route>
+        </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
