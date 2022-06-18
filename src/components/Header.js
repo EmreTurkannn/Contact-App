e3c21e46d95a657b20ed927f2ae40a16d88c19f3
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-19 01:57:06 +0300

    location variable created with useLocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React,{useState,useEffect} from 'react'
-import {Link,useLocation} from "react-router-dom"
+import {Link,useLocation,useParams} from "react-router-dom"
 import "./Header.css"
 const Header = () => {
 
     const [activeTab,setActiveTab]=useState("Home");
-
+    const location=useLocation();
+    useEffect(()=>{
+        if(location.pathname==="/"){
+            setActiveTab("Home");
+        }
+        else if(location.pathname==="/add"){
+            setActiveTab("AddContact");
+        }
+        else if(location.pathname==="/about"){
+            setActiveTab("About");
+        }
+    },[location])
 
   return (
     <div className='header'>
