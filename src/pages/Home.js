1504a53629e90642470aeb21f7b9ff7460c9819b
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-19 14:57:42 +0300

    Homejs component user table created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import fireDb from "../firebase"
+import {Link} from "react-router-dom"
+import "./Home.css"
+
 
 const Home = () => {
+
+  const [user,setUser]=useState({});
+  
+  useEffect(()=>{
+    fireDb.child("contacts").on("value",(snapshot)=>{
+      if(snapshot.val()!==null){
+        setUser({...snapshot.val()});
+      }else{
+        setUser({});
+      }
+
+    })
+    return ()=>{
+      setUser({});
+    }
+  },[]); 
+
   return (
-    <div>
-        <h2>Home</h2>
+    <div style={{marginTop:"100px"}}>
+      <table className='styled-tabled'>
+        <thead>
+          <tr>
+            <th style={{textAlign:"center"}}>No.</th>
+            <th style={{textAlign:"center"}}>Name</th>
+            <th style={{textAlign:"center"}}>Email</th>
+            <th style={{textAlign:"center"}}>Contact</th>
+            <th style={{textAlign:"center"}}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(user).map((id,index)=>{  //Object keys(user) ---> Object(user) return array 
+            return(
+              <tr key={id}>
+                <th scope="row">{index+1}</th>
+                <td>{user[id].name}</td>
+                <td>{user[id].email}</td>
+                <td>{user[id].contact}</td>
+              </tr>
+              )
+          })}
+        </tbody>
+      </table>
+        
     </div>
   )
 }
