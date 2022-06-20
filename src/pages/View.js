df079f3a7e73d7a831f262195153d6dcaaeedbff
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-20 03:49:38 +0300

    import fixed

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import fireDb from "../firebase"
-import { useParams,useNavigate,Link } from 'react-router'
+import { useParams,useNavigate,Link } from 'react-router-dom'
 import "./View.css"
 const View = () => {
   
