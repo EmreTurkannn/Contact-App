3d5603b82dcbd4e451261e939be2d7462159c10a
Emre_Turkan <emre_turkan_98@hotmail.com>
2022-06-21 03:31:33 +0300

    add searchData function

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,63 +1,72 @@
-import React,{useEffect,useState} from 'react'
-import { useLocation,Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useLocation, Link } from 'react-router-dom'
 import fireDb from "../firebase"
 import "./Search.css"
 
 const Search = () => {
 
-    const[data,setData]=useState({});
+  const [data, setData] = useState({});
 
-    const useQuery=()=>{
-        return new URLSearchParams(useLocation().search); 
-    }
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
 
-    useEffect(()=>{
-        searchData();
-    },[search])
+  let query = useQuery();
+  let search = query.get("name");
+  console.log("search", search);
 
-    const searchData=()=>{
-        fireDb
-        .child("contacts")
-        .orderByChild("name")
-        .equalTo(search)
-        .on("value",(snapshot)=>{
-            if(snapshot.val()){
-                const data=snapshot.val();
-                setData(data);
-            }
-        })
-    }
-    let query=useQuery();
-    let search=query.get("name");
-    console.log("search",search);
+  useEffect(() => {
+    searchData();
+  }, [search])
+
+  const searchData = () => {
+    fireDb
+      .child("contacts")
+      .orderByChild("name")
+      .equalTo(search)
+      .on("value", (snapshot) => {
+        if (snapshot.val()) {
+          const data = snapshot.val();
+          setData(data);
+        }
+      })
+  }
+
   return (
     <>
-    <div style={{ marginTop: "100px" }}>
-      <table className='styled-tabled'>
-        <thead>
-          <tr>
-            <th style={{ textAlign: "center" }}>No.</th>
-            <th style={{ textAlign: "center" }}>Name</th>
-            <th style={{ textAlign: "center" }}>Email</th>
-            <th style={{ textAlign: "center" }}>Contact</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(data).map((id, index) => {  //Object keys(data) ---> Object(data) return array 
-            console.log(id);
-            return (
-              <tr key={id}>
-                <th scope="row">{index + 1}</th>
-                <td>{data[id].name}</td>
-                <td>{data[id].email}</td>
-                <td>{data[id].contact}</td>
-                
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <div style={{ marginTop: "100px" }}>
+        {Object.keys(data).length === 0 ? (
 
+          <h2>No Search Found with That Name : {query.get("name")}</h2>
+        ) : (
+          <table className='styled-tabled'>
+            <thead>
+              <tr>
+                <th style={{ textAlign: "center" }}>No.</th>
+                <th style={{ textAlign: "center" }}>Name</th>
+                <th style={{ textAlign: "center" }}>Email</th>
+                <th style={{ textAlign: "center" }}>Contact</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(data).map((id, index) => {  //Object keys(data) ---> Object(data) return array 
+                console.log(id);
+                return (
+                  <tr key={id}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{data[id].name}</td>
+                    <td>{data[id].email}</td>
+                    <td>{data[id].contact}</td>
+
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+
+        )}
+
+     
     </div>
     </>
   )
